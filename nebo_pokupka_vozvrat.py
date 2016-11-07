98cd5294e14a57fa36f6f2ac0f601a0875ab2651
Mark M <maltsevmark@gmail.com>
2016-11-07 23:10:22 +0300

    added xml!
    fixed articul and strich_code extraction

--- nebo_pokupka_vozvrat.py
+++ nebo_pokupka_vozvrat.py
@@ -1,4 +1,6 @@
-import xlrd, re, os, string, pprint
+# -*- coding: cp1251 -*-
+
+import xlrd, re, os, string, pprint, string, codecs
 import openpyxl as op
 
 def saveIntoWb(tables): #type=dict
@@ -50,7 +52,7 @@ def extractTable(table, cols, cond_col, condition): #type(table)=list, type(cols
     i = table[0]
     for c in cols:
         head.append(i[ci[c]])
-    result.append(head)
+    result.append(head+[u'strich_code',u'articul'])
     
     for i in table:
         if i[ci[cond_col]] == condition:
@@ -71,12 +73,60 @@ def extractTable(table, cols, cond_col, condition): #type(table)=list, type(cols
             pass
     return result
 
+def removePunctuation(line):
+    exclude = string.punctuation
+    exclude_ = exclude.replace('-','')
+    exclude = exclude_.replace('_','')
+    exclude += ' '
+    s = ''.join(ch for ch in line if ch not in exclude)
+    return s
+
+def getStrichCode(table): #type=list
+    newtab = []
+    for row in table:
+        m = re.search(u'(шк(.+))', row[-1])
+        n = re.search(u'(арт([^ш]+))', row[-1])
+        if m is not None:
+            strich = removePunctuation(m.group(2))
+            row.append(strich)
+        else:
+            row.append('')
+        if n is not None:
+            articul = removePunctuation(n.group(2))
+            row.append(articul)
+        else:
+            row.append('')
+        newtab.append(row)
+    return newtab
+
+def xmlifySheet(table): # type = list    
+    result = u'<xml>\r\n'
+    l = table[1:]
+    for i in l:
+        result += u'\t<stock>\r\n'
+        result += u'\t\t<shipment_number>%s</shipment_number>\r\n'%i[0]
+        result += u'\t\t<cost_per_one>%s</cost_per_one>\r\n'%i[1]
+        result += u'\t\t<money_received>%s</money_received>\r\n'%i[2]
+        result += u'\t\t<status>%s</status>\r\n'%i[3]
+        result += u'\t\t<status_date>%s</status_date>\r\n'%i[4]
+        result += u'\t\t<stock_name>%s</stock_name>\r\n'%i[5]
+        result += u'\t\t<strich_code>%s</strich_code>\r\n'%i[6]
+        result += u'\t\t<articul>%s</articul>\r\n'%i[7]
+        result += u'\t</stock>\r\n'
+    result += u'</xml>\r\n'
+    return result
+        
 
 if __name__ == '__main__':
     table_arr = getTable('source.xls')
     d = {}
-    d['pokupka'] = extractTable(table_arr, 'aghlmr', 'l', u'Р’СЂСѓС‡РµРЅ')
-    d['vozvrat'] = extractTable(table_arr, 'aghlmrpqt', 'p', u'Р’РѕР·РІСЂР°С‚')
+    d['pokupka'] = getStrichCode(extractTable(table_arr, 'aghlmr', 'l', u'Вручен'))
+    d['vozvrat'] = extractTable(table_arr, 'aghlmrpqt', 'p', u'Возврат')
     saveIntoWb(d)
+    xml = xmlifySheet(d['pokupka'])
+    f = codecs.open('pokupka_xml.xml', 'w', 'utf8')
+    f.write(xml)
+    f.close()
+    
     
     
